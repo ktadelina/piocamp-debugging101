5ab5b408a8d305591ebb8f4781e313629d90e995
Catalina Zapata <ktadelina@184.2.17.175.adsl-pool.jlccptt.net.cn>
2018-11-26 20:39:19 -0500

    estamos en el cuatro y vamos pal cinco

--- challenges/4.js
+++ challenges/4.js
@@ -2,6 +2,6 @@
 Corrige los pares faltantes en el código.
 */
 
-let myArray = [1, 2, 3;
-let arraySum = myArray.reduce((previous, current =>  previous + current);
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
 console.log(`Sum of array values is: ${arraySum}`);
